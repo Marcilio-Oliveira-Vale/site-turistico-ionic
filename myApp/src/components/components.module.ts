6a024d9dee584adff8da32d377ff74c522b841ca
Marcilio-Oliveira-Vale <89142492+Marcilio-Oliveira-Vale@users.noreply.github.com>
2021-10-10 11:12:08 -0300

    Site turistico em ionic

---
+++ myApp/src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { SobreSobreComponent } from './sobre-sobre/sobre-sobre';
+@NgModule({
+	declarations: [SobreSobreComponent],
+	imports: [],
+	exports: [SobreSobreComponent]
+})
+export class ComponentsModule {}
